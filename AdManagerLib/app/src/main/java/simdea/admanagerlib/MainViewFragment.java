8e76070e345e03f7350b497a2df7936c96134e6e
Carlos Peixoto <carlosandradepeixoto@gmail.com>
2015-08-10 20:34:39 +0100

    [Supoprt] - Google Advertisement BANNER done

--- AdManagerLib/app/src/main/java/simdea/admanagerlib/MainViewFragment.java
+++ AdManagerLib/app/src/main/java/simdea/admanagerlib/MainViewFragment.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
-import simdea.admanager.Manager;
+import simdea.admanager.AdManager;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -22,7 +22,14 @@ public class MainViewFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_main_view, container, false);
 
-        Manager layout = (Manager) v.findViewById(R.id.admanager);
+        FrameLayout layout = (FrameLayout) v.findViewById(R.id.admanager);
+
+        //Initialize Manager
+        AdManager adManager = new AdManager(getActivity(), null);
+
+
+        //Inject Manager view to App layout
+        layout.addView(adManager.getRootView());
 
 
         return v;
